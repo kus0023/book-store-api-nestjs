f9e0a707b14ffb20a0b109aee0e1a7878f4ca9cd
kus0023 <utsav.singh0023@gmail.com>
2025-03-30 21:39:43 +0530

    Implemented database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, BooksModule, OrdersModule, AuthModule],
+  imports: [UsersModule, BooksModule, OrdersModule, AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
