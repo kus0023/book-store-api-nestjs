7d6ae05b41fac428c59e76c881df2e377c228884
kus0023 <utsav.singh0023@gmail.com>
2025-04-01 21:52:54 +0530

    Implemented Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { AllExceptionsFilter } from './all-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function bootstrap() {
   app.useLogger(app.get(Logger));
 
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Book Store API')
+    .setDescription('Search and Order Books from this API.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
